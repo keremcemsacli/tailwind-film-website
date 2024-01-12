eaefd9e12025c0c4b9d6a0c4a4325b78f115a2d8
Keremcem Saçlı <05keremcemsacli@gmail.com>
2024-01-12 19:38:28 +0300

    finish project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./public/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      screens: {
+        lg: '1140px',
+        xl: '1140px',
+        '2xl': '1140px',
+      },
+    },
+    extend: {
+      fontFamily: {
+        gemunu: ['Gemunu Libre', 'sans-serif'],
+        open: ['Open Sans', 'sans-serif'],
+      },
+      colors: {
+        'gega-red': '#861d1d',
+        'gega-melon': '#FFD369',
+        'gega-grey': '#DDDDDD',
+        'gega-white': '#F7F7F7',
+      },
+
+      spacing: {
+        '128': '32rem',
+      }
+    },
   },
   plugins: [],
 }
